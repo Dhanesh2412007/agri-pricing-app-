7f52fa054d129a4f9e3ec4889acffbbbca3f008d
Dhanesh2412007 <ypdaneshreddyreddy@gmail.com>
2025-12-14 19:52:37 +0530

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+// server/models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    // **KEY FOR ROLE-BASED ACCESS**
+    role: { 
+        type: String, 
+        enum: ['customer', 'partner'], 
+        default: 'customer' 
+    },
+    farmName: { 
+        type: String, 
+        required: function() { return this.role === 'partner'; } 
+    }
+});
+
+// Middleware to hash password before saving
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// Method to compare passwords
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
